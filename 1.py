14f17437bf25b1aab38ae87f0383ec289cab64b8
Atikur Rahaman <atikur@rahaman.noreply.github.com>
2024-02-16 15:41:23 +0600

    first commit

---
+++ 1.py
@@ -0,0 +1,3 @@
+"""
+    Make a class named Star_Cinema which will have one class attribute named hall_list which is an empty list initially. Make a method named entry_hall() to insert an object of class Hall (Described below) inside its hall_list. 	
+"""
